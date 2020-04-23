0a98c247355df95e61fc20061e666e9c44af86d9
Alberto Schiabel <skiabo97@gmail.com>
2020-04-23 12:28:36 +0200

    Added Continuous Integration scripts to .ci folder

---
+++ .ci/install_linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ev
+
+OS=linux
+ARCH=x86_64
+
+# install bazel
+GH_BASE="https://github.com/bazelbuild/bazel/releases/download/$V_BAZEL"
+GH_ARTIFACT="bazel-$V_BAZEL-installer-$OS-$ARCH.sh"
+URL="$GH_BASE/$GH_ARTIFACT"
+echo $URL
+wget -O install_bazel.sh $URL
+chmod +x install_bazel.sh
+./install_bazel.sh --user
+rm -f install_bazel.sh
+
+# install clang-format
+sudo apt install clang-format
+clang-format --version
